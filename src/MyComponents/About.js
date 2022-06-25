9addd8a1b151b96410f153505b27b66de2533d12
Vipin Gautam <gautamvipin2002@gmail.com>
2022-06-26 01:08:56 +0530

    Intial Commit

---
+++ src/MyComponents/About.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const About = ()=> {
+  return (
+    <div>This is About content
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Obcaecati doloribus enim quidem incidunt impedit sit minus optio alias? Ducimus nam repellendus suscipit dolorem illum deserunt nemo quibusdam explicabo tempore? Quos voluptatibus, tenetur nam explicabo in quam dolorum doloremque. Ea veniam beatae ad eligendi neque distinctio repellat natus ipsam, magni in, laudantium earum. Corporis, voluptatibus blanditiis.</p>
+    </div>
+  )
+}
---
+++ src/MyComponents/AddTodo.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+
+export const AddTodo = (props) => {
+    const [title, settitle] = useState("");
+    const [desc, setdesc] = useState("");
+    const submit = (e)=>{
+        e.preventDefault();
+        if(!title || !desc){
+            alert("Title or Description cannot be blank")
+        } else{
+        props.addTodo(title, desc);
+        settitle("");        
+        setdesc("");
+        }
+    }
+  return (
+    <div className="container my-3">
+     <h3>Add a Todo</h3>
+        <form onSubmit={submit}>
+            <div className="mb-3">
+                <label htmlFor="title" className="form-label">To-do Title</label>
+                <input type="text" value={title} onChange={(e)=>{settitle(e.target.value)}} className="form-control" id="title" aria-describedby="emailHelp"/>
+            </div>
+            <div className="mb-3">
+                <label htmlFor="desc" className="form-label">Todo Description</label>
+                <input type="text" value={desc} onChange={(e)=>{setdesc(e.target.value)}} className="form-control" id="desc"/>
+            </div>
+            <button type="submit" className="btn btn-sm btn-success">Submit</button>
+        </form>
+    </div>
+  )
+}
